7ca20520818ffd6b985c70c68963b718ad00dafc
Wayne De Los Angeles <wayne.delosangeles@ph.softvision.global>
2022-06-24 15:35:45 +0800

    Starting codes for TDD with SCM

---
+++ src/main/java/com/example/SFT/Academy/TDD/with/SCM/SftAcademyTddWithScmApplication.java
@@ -0,0 +1,13 @@
+package com.example.SFT.Academy.TDD.with.SCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftAcademyTddWithScmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftAcademyTddWithScmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SFT/Academy/TDD/with/SCM/SftAcademyTddWithScmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SFT.Academy.TDD.with.SCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftAcademyTddWithScmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
